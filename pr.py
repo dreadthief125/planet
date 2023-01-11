76a89055f6ddd3f97e03f8bd85f90b2586ee6ecd
dreadthief125 <88696805+dreadthief125@users.noreply.github.com>
2023-01-11 15:13:25 -0800

    Add files via upload

---
+++ pr.py
@@ -0,0 +1,15 @@
+import cv2
+img=cv2.imread('solar-system.jpg')
+
+cv2.putText(img,'Sun',(100,80),cv2.FONT_HERSHEY_PLAIN,2,(0,0,255))
+cv2.putText(img,'Mercury',(110,180),cv2.FONT_HERSHEY_PLAIN,2,(0,0,255))
+cv2.putText(img,'Venus',(190,270),cv2.FONT_HERSHEY_PLAIN,2,(0,0,255))
+cv2.putText(img,'Earth',(300,2700),cv2.FONT_HERSHEY_PLAIN,2,(0,0,255))
+cv2.putText(img,'Mars',(400,270),cv2.FONT_HERSHEY_PLAIN,2,(0,0,255))
+cv2.putText(img,'Jupiter',(590,80),cv2.FONT_HERSHEY_PLAIN,2,(0,0,255))
+cv2.putText(img,'Saturn',(720,110),cv2.FONT_HERSHEY_PLAIN,2,(0,0,255))
+cv2.putText(img,'Uranus',(950,130),cv2.FONT_HERSHEY_PLAIN,2,(0,0,255))
+cv2.putText(img,'Neptune',(1080,130),cv2.FONT_HERSHEY_PLAIN,2,(0,0,255))
+cv2.imshow('Planets',img)
+cv2.waitKey(0)
+cv2.imwrite('Solar-system-with-name.jpg',img)
